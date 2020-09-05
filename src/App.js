08b8836117c7790cae44f2a040ce4c84bc8a21f4
rwaitheras <waitherarose50@gmail.com>
2020-09-05 06:20:57 +0300

    issues

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App__Aside"></div>
+      <div className="App__Form">
+      <div className="PageSwitcher">
+         <a href="#" className="PageSwitcher_Item">Sign In</a>
+         <a href="#" className="PageSwitcher_Item PageSwitcher__Item--Active">Sign Up</a>
+         </div>
+
+         <div className="FormTitle">
+           <a href="#" className="FormTitle__link">Sign In</a> or <a href="#"
+           className="FormTitle__Link FormTitle__Link--Active">Sign Up</a>
+         </div>
+
+      <div className="FormCenter">
+       <form className="FormFields" onSubmit={this.handleSubmit}>
+        <div className="FormField">
+          <label className="FormField__Label" htmlFor="name">Full Name</label>
+          <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name"/>
+        </div>
+        </form>
+      </div>
+
+        </div>
     </div>
   );
 }
+
+
+
 
 export default App;
